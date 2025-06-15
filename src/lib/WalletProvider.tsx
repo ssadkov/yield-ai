a608ec0aae035c7768870f280dc71e53e9648fb8
ssadkov <mail@sheremetev.info>
2025-06-15 13:38:34 +0500

    Refactor WalletProvider to remove automatic wallet derivation for Ethereum and Solana, and update network configuration from TESTNET to MAINNET for AptosWalletAdapterProvider.

--- src/lib/WalletProvider.tsx
+++ src/lib/WalletProvider.tsx
@@ -1,23 +1,9 @@
 "use client";
 
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
-import { setupAutomaticEthereumWalletDerivation } from "@aptos-labs/derived-wallet-ethereum";
-import { setupAutomaticSolanaWalletDerivation } from "@aptos-labs/derived-wallet-solana";
 import { PropsWithChildren } from "react";
 import { Network } from "@aptos-labs/ts-sdk";
 import { useToast } from "@/components/ui/use-toast";
-
-const searchParams =
-  typeof window !== "undefined"
-    ? new URL(window.location.href).searchParams
-    : undefined;
-const deriveWalletsFrom = searchParams?.get("deriveWalletsFrom")?.split(",");
-if (deriveWalletsFrom?.includes("ethereum")) {
-  setupAutomaticEthereumWalletDerivation({ defaultNetwork: Network.TESTNET });
-}
-if (deriveWalletsFrom?.includes("solana")) {
-  setupAutomaticSolanaWalletDerivation({ defaultNetwork: Network.TESTNET });
-}
 
 let dappImageURI: string | undefined;
 if (typeof window !== "undefined") {
@@ -31,7 +17,7 @@ export const WalletProvider = ({ children }: PropsWithChildren) => {
     <AptosWalletAdapterProvider
       autoConnect={true}
       dappConfig={{
-        network: Network.TESTNET,
+        network: Network.MAINNET,
         aptosApiKeys: {
           testnet: process.env.NEXT_PUBLIC_APTOS_API_KEY_TESTNET,
           devnet: process.env.NEXT_PUBLIC_APTOS_API_KEY_DEVNET,
