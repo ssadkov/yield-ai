caccb3fb1a7b760112ff5268dc1a4c689cd443b4
Rustam Galiev <orion009@bk.ru>
2026-02-24 22:07:33 +0500

    feat(*): common cards

---
+++ src/shared/ProtocolCard/index.ts
@@ -0,0 +1,8 @@
+export { ProtocolCard } from "./ProtocolCard";
+export type { ProtocolCardProps } from "./ProtocolCard";
+export { ProtocolCardSkeleton } from "./ProtocolCardSkeleton/ProtocolCardSkeleton";
+export type { ProtocolCardSkeletonProps } from "./ProtocolCardSkeleton/ProtocolCardSkeleton";
+export { ProtocolCardPosition } from "./ProtocolCardPosition/ProtocolCardPosition";
+export type { ProtocolCardPositionProps } from "./ProtocolCardPosition/ProtocolCardPosition";
+export type { ProtocolPosition } from "./types";
+export { PositionBadge } from "./types";
